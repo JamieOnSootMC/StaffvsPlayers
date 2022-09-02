929bee4ad5f1fe3d36c02b719b5254218a49c3fa
JamieOnSootMC <jamie@sootmc.net>
2022-09-02 18:09:38 +0100

    Add PlayerRespawnEvent

--- src/main/java/net/sootmc/staffvsplayers/DeathListener.java
+++ src/main/java/net/sootmc/staffvsplayers/DeathListener.java
@@ -1,9 +1,12 @@
 package net.sootmc.staffvsplayers;
 
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class DeathListener implements Listener {
 
@@ -16,5 +19,16 @@ public class DeathListener implements Listener {
             StaffvsPlayers.database.setKiller(event.getEntity().getName(), event.getEntity().getKiller().getName());
         }
 
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        if(player.hasPermission("svp.staff")) {
+            return;
+        }
+
+        player.setGameMode(GameMode.SPECTATOR);
+        player.sendMessage(ChatColor.GOLD + "Soot" + ChatColor.RED + "MC" + ChatColor.RESET + " | " + ChatColor.RED + "You have died! Please make sure you stay in spectator as to not interfere with the rest of the game!");
     }
 }
